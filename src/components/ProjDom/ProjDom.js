4a7e6b02d787e15588907211ff65aed3e763cb7b
dona-murmu <109920996+coding-junction@users.noreply.github.com>
2023-09-25 15:42:47 +0530

    changes made in project domains

--- src/components/ProjDom/ProjDom.js
+++ src/components/ProjDom/ProjDom.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import {motion, spring} from "framer-motion";
-// import { useState } from 'react';
 import './ProjDom.css'
 
 export default function ProjDom() {
@@ -8,20 +6,16 @@ export default function ProjDom() {
   const[isOpen, setIsOpen] = useState(false);
   return (
     <div className='ProjDom'>
-      <div className='projDom-head'>Project Domains</div>
-        < motion.div 
-          transition={{ layout: { duration: 0.1, type: "spring"}}} 
-          layout 
-          onClick={() => setIsOpen(!isOpen)} 
-          className='cards'>
-          <motion.h2 layout="position" className='card-title'>Web Development
-            {isOpen &&
-              <motion.div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quasi debitis nam, exercitationem ad unde accusamus maiores asperiores amet saepe odit voluptatibus recusandae quibusdam sequi quaerat soluta ab maxime voluptates.</p>
-              </motion.div>
-            }
-          </motion.h2>
-        </motion.div>
+      <div className='projDom-head'>
+        Project Domains
+      </div>
+      <div className='projDoms-domains'>
+        <span className='domains-card'>Web Dev</span>
+        <span className='domains-card'>Android</span>
+        <span className='domains-card'>C/C++</span>
+        <span className='domains-card'>AI/ML</span>
+      </div>
+        
     </div>
   )
 }
