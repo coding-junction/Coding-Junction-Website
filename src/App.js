9e697b754920128a5b4025ff5844c4d53335087d
dona-murmu <109920996+coding-junction@users.noreply.github.com>
2023-10-03 09:52:06 +0530

    Made hamburg menu

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import WebCursor from './components/WebCursor/WebCursor';
 import UnderBuild from './components/UnderBuild/UnderBuild'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
-import Gallery from './components/Gallery/Gallery';
+// import Gallery from './components/Gallery/Gallery';
 
 function App() {
   return (
@@ -24,6 +24,7 @@ function App() {
                 <Landing />
                 <AboutUs />
                 <ProjDom />
+                {/* <Gallery/> */}
               </>
             } />
             <Route path = "/underConstruction" element={
@@ -32,51 +33,6 @@ function App() {
           </Routes>
         <Footer />
       </BrowserRouter>
-      <Particle />
-      <AnimatedCursor 
-        innerSize={8}
-        outerSize={35}
-        color='255, 255, 255'
-        outerAlpha={0.2}
-        innerScale={1}
-        outerScale={2}
-        hasBlendMode={true} 
-        innerStyle={{
-          backgroundColor: 'var(--cursor-color)'
-        }}
-        outerStyle={{
-          mixBlendMode: 'exclusion'
-        }}
-        clickables={[
-          'a',
-          'input[type="text"]',
-          'input[type="email"]',
-          'input[type="number"]',
-          'input[type="submit"]',
-          'input[type="image"]',
-          'label[for]',
-          'select',
-          'textarea',
-          'button',
-          '.link',
-          {
-            target: '.custom',
-            options: {
-              innerSize: 12,
-              outerSize: 12,
-              color: '255, 255, 255',
-              outerAlpha: 0.3,
-              innerScale: 0.7,
-              outerScale: 5
-            }
-          }
-        ]}
-      />
-      <Nav />
-      <Landing />
-      <AboutUs />
-      <ProjDom />
-      <Gallery/>
     </div>
   );
 }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,48 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Nav.css";
 
 export default function Nav() {
+
+  const [showMenu, setShowMenu] = useState(false);
+
+
   return (
+    <>
     <div className="nav">
       <div className="logo">
-        <img src={ require("../../Assets/Images/club-logo.png") } alt="" />
+        <img src={require("../../Assets/Images/club-logo.png")} />
       </div>
       <div className="nav-buttons">
         <ul>
           <li>
-          <Link to = '/'>
-            <button>HOME</button>
-          </Link>
+            <Link to='/'>
+              <button>HOME</button>
+            </Link>
           </li>
           <li>
-            <Link to = '/underConstruction'>
+            <Link to='/underConstruction'>
               <button>APP</button>
             </Link>
           </li>
           <li>
-            <Link to = '/underConstruction'>
+            <Link to='/underConstruction'>
               <button>OUR TEAM</button>
             </Link>
           </li>
           <li>
-            <Link to = '/underConstruction'>
+            <Link to='/underConstruction'>
               <button>CONTACT US</button>
             </Link>
           </li>
           <li>
-            <Link to = '/underConstruction'>
+            <Link to='/underConstruction'>
               <button className="register-btn">REGISTER</button>
             </Link>
           </li>
           <li>
-            <Link to = '/underConstruction'>
+            <Link to='/underConstruction'>
               <button className="login-btn">LOGIN</button>
             </Link>
           </li>
         </ul>
       </div>
     </div>
+    <div className="nav-burger">
+        {showMenu ?
+          <>
+            <div>
+              <button onClick={() => {
+                setShowMenu(!showMenu)
+              }}>&#8904;</button>
+            </div>
+            <div className="burger-menu">
+              <ul>
+                <li>
+                  <Link to='/'>
+                    <button>HOME</button>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/underConstruction'>
+                    <button>APP</button>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/underConstruction'>
+                    <button>OUR TEAM</button>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/underConstruction'>
+                    <button>CONTACT US</button>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/underConstruction'>
+                    <button className="register-btn">REGISTER</button>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/underConstruction'>
+                    <button className="login-btn">LOGIN</button>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </>
+          :
+          <button onClick={() => {
+            setShowMenu(!showMenu)
+          }}>&equiv;</button>
+        }
+    </div>
+    </>
   );
 }
