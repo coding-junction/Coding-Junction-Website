6549c2b949d49f88d712f4b5ebf968205368db7a
dona-murmu <109920996+coding-junction@users.noreply.github.com>
2023-10-03 09:51:43 +0530

    Made hamburg menu + minor tweaks

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import WebCursor from './components/WebCursor/WebCursor';
 import UnderBuild from './components/UnderBuild/UnderBuild'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
+import Gallery from './components/Gallery/Gallery';
 
 function App() {
   return (
@@ -31,6 +32,51 @@ function App() {
           </Routes>
         <Footer />
       </BrowserRouter>
+      <Particle />
+      <AnimatedCursor 
+        innerSize={8}
+        outerSize={35}
+        color='255, 255, 255'
+        outerAlpha={0.2}
+        innerScale={1}
+        outerScale={2}
+        hasBlendMode={true} 
+        innerStyle={{
+          backgroundColor: 'var(--cursor-color)'
+        }}
+        outerStyle={{
+          mixBlendMode: 'exclusion'
+        }}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link',
+          {
+            target: '.custom',
+            options: {
+              innerSize: 12,
+              outerSize: 12,
+              color: '255, 255, 255',
+              outerAlpha: 0.3,
+              innerScale: 0.7,
+              outerScale: 5
+            }
+          }
+        ]}
+      />
+      <Nav />
+      <Landing />
+      <AboutUs />
+      <ProjDom />
+      <Gallery/>
     </div>
   );
 }
