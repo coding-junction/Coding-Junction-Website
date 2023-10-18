a66a1830ac596a40bae65eafe0cc362e077e8cd3
The15thSin <ayushjalan1203@gmail.com>
2023-10-19 03:04:46 +0530

    Minor bugs fixed (Project cards)

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        {/* <Particle /> */}
+        <Particle />
         <WebCursor />
         <Nav />
           <Routes>
