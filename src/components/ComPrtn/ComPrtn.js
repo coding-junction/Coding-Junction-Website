24e823bf45d4fe9bd5cd0e6ee3cd08a22808f1aa
ranadebsaha <ranadebsaha2002@gmail.com>
2024-08-24 20:10:14 +0530

    landing bug fix

---
+++ src/components/ComPrtn/ComPrtn.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './ComPrtn.css'
+import { Link } from 'react-router-dom';
+import prtnArr from './PrtnArray.json'
+
+function UpcomingEvents() {
+
+  return (
+    <div className='comprtn'>
+      <div className="comprtn-head">
+        <h1>Community Partners</h1>
+      </div>
+      </div>
+  );
+}
+
+export default UpcomingEvents;
+
