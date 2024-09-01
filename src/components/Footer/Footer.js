4ecbf1891739008b08b277c13ea94eac891b079d
Coding Junction <109920996+coding-junction@users.noreply.github.com>
2024-09-01 19:31:24 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -54,7 +54,7 @@ export default function Footer() {
             </Link>
           </div>
          <div className='brandassets'>
-            <Link to='/brandassets'>
+            <a href="https://bit.ly/cj-brand-assets" target="_blank"></a>
               <button>Brand Assets</button>
             </Link>
           </div>
